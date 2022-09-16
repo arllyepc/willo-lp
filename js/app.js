bfa26498395bcaf7c03b5040b4173e60a9878e6d
Arlly Cavalcanti <arllyepc@gmail.com>
2022-09-16 19:36:22 -0300

    otimização de imagens

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const logo = document.querySelector('.logo-desktop');
 const linksMobile = document.querySelectorAll('nav.navbar-mobile ul li');
 const linksDesktop = document.querySelectorAll('ul.menu-desktop li');
 const sections = document.querySelectorAll('section');
-console.log(linksDesktop);
-console.log(sections)
 
 window.addEventListener('scroll', () => {
 	let current = '';
@@ -15,14 +13,11 @@ window.addEventListener('scroll', () => {
 		const sectionHeight = section.clientHeight;
 		if (scrollY > sectionTop - sectionHeight / 3) {
 			current = section.getAttribute('id');
-			console.log(current);
 		}
 	});
 
 	linksDesktop.forEach((linkDesktop) => {
-		linkDesktop.firstElementChild.classList.remove("active");
-			
-		
+		linkDesktop.firstElementChild.classList.remove('active');
 
 		if (linkDesktop.classList.contains(current)) {
 			linkDesktop.firstElementChild.classList.add('active');
@@ -30,9 +25,7 @@ window.addEventListener('scroll', () => {
 	});
 
 	linksMobile.forEach((linkMobile) => {
-		linkMobile.firstElementChild.classList.remove("active");
-			
-		
+		linkMobile.firstElementChild.classList.remove('active');
 
 		if (linkMobile.classList.contains(current)) {
 			linkMobile.firstElementChild.classList.add('active');
@@ -42,7 +35,6 @@ window.addEventListener('scroll', () => {
 
 linksMobile.forEach((el) => {
 	el.addEventListener('click', (event) => {
-		
 		menuBtn.classList.toggle('open');
 		navbarMobile.classList.toggle('active');
 		logo.classList.toggle('active');
